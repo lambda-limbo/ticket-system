3c2c41f96986895b02d1a596c02e1735c9c29a8e
Rafael Campos Nunes <rafaelnunes@engineer.com>
2020-08-18 18:06:02 -0300

    Remove useless empty lines
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/org/ticket/view/Fonts.java
+++ src/org/ticket/view/Fonts.java
@@ -10,6 +10,4 @@ public class Fonts {
     public static Font medium = new Font(name, Font.CENTER_BASELINE, 17);
     public static Font big = new Font(name, Font.CENTER_BASELINE, 25);
     public static Font huge = new Font(name, Font.CENTER_BASELINE, 35);
-
-
 }
