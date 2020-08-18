12dd02b67d52f9401c26444f3e9e4810391352ea
Rafael Campos Nunes <rafaelnunes@engineer.com>
2020-08-18 18:06:51 -0300

    Improve the docs by a tiny bit
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/org/ticket/model/dao/GenericDAO.java
+++ src/org/ticket/model/dao/GenericDAO.java
@@ -8,27 +8,54 @@ import javax.transaction.NotSupportedException;
 public class GenericDAO<T> implements IGenericDAO<T> {
 
     EntityManager manager = JPAConn.instance();
-
+    
+    /**
+     * @brief Saves an object of type T to the database
+     * @param object
+     * @throws PersistenceException
+     */
     public void save(T object) throws PersistenceException {
         manager.getTransaction().begin();
         manager.persist(object);
         manager.getTransaction().commit();
     }
-
+    
+    /**
+     * @brief Searches for an object with the given params
+     * @param objClass
+     * @param fieldName
+     * @param argument
+     * @return The retrieved object
+     * @throws PersistenceException
+     */
     public T search(Class objClass, String fieldName, String argument) throws PersistenceException {
         String query_string = "SELECT t FROM " + objClass.getTypeName() + " t WHERE " + fieldName + " = \'" + argument + "\'";
         Query query = manager.createQuery(query_string);
         Object object = query.getSingleResult();
         return (T) object;
     }
-
+    
+    /**
+     * @brief
+     * @param objClass
+     * @param fieldName
+     * @param argument
+     * @return A single object of type T
+     * @throws PersistenceException
+     */
     public T search(Class objClass, String fieldName, int argument) throws PersistenceException  {
         String query_string = "SELECT t FROM " + objClass.getTypeName() + " t WHERE " + fieldName + " = " + argument;
         Query query = manager.createQuery(query_string);
         Object object = query.getSingleResult();
         return (T) object;
     }
-
+    
+    /**
+     * @brief Searches for a given object
+     * @param objClass
+     * @return A list of objects corresponding to the search
+     * @throws PersistenceException
+     */
     public List<T> search(Class objClass) throws PersistenceException  {
         String query_string = "SELECT t FROM " + objClass.getTypeName() + " t";
         Query query = manager.createQuery(query_string);
@@ -43,7 +70,7 @@ public class GenericDAO<T> implements IGenericDAO<T> {
      * @param updateFieldName
      * @param value The new value to be set on fieldName
      * @param pkey The pkey of the row to be updated.
-     * @return The new object updated
+     * @return The ID of the updated object
      */
     public int update(Class objClass, String updateFieldName, String value, int pkey)
             throws PersistenceException {
@@ -59,7 +86,12 @@ public class GenericDAO<T> implements IGenericDAO<T> {
 
         return ret;
     }
-
+    
+    /**
+     * Updates object
+     * @param object
+     * @return
+     */
     public int update(T object) {
         manager.getTransaction().begin();
         manager.refresh(object);
@@ -67,7 +99,13 @@ public class GenericDAO<T> implements IGenericDAO<T> {
 
         return 1;
     }
-
+    
+    /**
+     * @brief Deletes object of type T
+     * @param object
+     * @return
+     * @throws PersistenceException
+     */
     public int delete(T object) throws PersistenceException {
         manager.getTransaction().begin();
         manager.remove(object);
